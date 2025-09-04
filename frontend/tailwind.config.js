34b054875ca5238df95ac5b9e73662840c1e72cf
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2025-09-04 02:56:13 -0400

    Create:  {Creating config files for tailwind framework}

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {
+          colors: {
+        brand: "#4472C4",
+      },
+  } },
+  plugins: [],
+}
