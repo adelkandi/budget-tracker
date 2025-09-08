fea74e00b106a90ef024c3deaa8f5d21d77fdb30
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2025-09-08 01:03:08 -0400

    Update:{Organizing code}

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/register"
-
+import Dashboard from "./pages/dashboard";
 function app(){
 
-    // return (<Register />)
+    
     return (
         <Router>
-            
+    
                 <Routes>
                     <Route path="/" element={<Login />}></Route>
                     <Route path="/register" element={<Register />}></Route>
+                    <Route path="/dashboard" element={<Dashboard />}></Route>
                 </Routes>
             
         </Router>
