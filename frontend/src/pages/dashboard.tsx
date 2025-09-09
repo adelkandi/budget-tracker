ea1cd3dfe21fcfb02044a2425d3024c9395c3459
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2025-09-09 17:26:35 -0400

    Update: {Adding card components}

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,11 +1,20 @@
 // Import components:
-import Navbar from "../components/Navbar"
+import Navbar from "../components/layout/Navbar"
+import IncomeCard from "../components/cards/IncomeCard"
+import BalanceCard from "../components/cards/BalanceCard"
+import ExpenseCard from "../components/cards/ExpenseCard"
 
 
 function Dashboard(){
     return (
-        <Navbar />
-        
+        <>
+            <Navbar />
+            <div className="dashboard-container">
+                <IncomeCard />
+                <ExpenseCard />
+                <BalanceCard />
+            </div>
+        </> 
     );
 }
 
