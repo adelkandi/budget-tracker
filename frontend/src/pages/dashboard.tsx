b2aa5dc3a0ed2d6a2e2ee23b5684dc83916c448a
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2025-09-10 23:01:24 -0400

    Mod : {Styling dashboard container div - flex and gap }

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -3,17 +3,18 @@ import Navbar from "../components/layout/Navbar"
 import IncomeCard from "../components/cards/IncomeCard"
 import BalanceCard from "../components/cards/BalanceCard"
 import ExpenseCard from "../components/cards/ExpenseCard"
-
+import IncomeExpensesChart from "../components/charts/IncomeExpensesChart"
 
 function Dashboard(){
     return (
         <>
             <Navbar />
-            <div className="dashboard-container">
+            <div className="dashboard-container flex justify-center gap-6">
                 <IncomeCard />
                 <ExpenseCard />
                 <BalanceCard />
             </div>
+            <IncomeExpensesChart />
         </> 
     );
 }
