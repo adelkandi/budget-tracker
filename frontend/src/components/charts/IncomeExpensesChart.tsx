6605a0124799b933f110d8365f9c5624e8c1ebc9
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2025-09-16 22:32:18 -0400

    Add: { Adding the chart data, then showing it as a Line }

--- frontend/src/components/charts/IncomeExpensesChart.tsx
+++ frontend/src/components/charts/IncomeExpensesChart.tsx
@@ -21,11 +21,53 @@ ChartJS.register(
     Legend
 )
 
+// Plain object {Test}:
+
+const chartData = {
+    labels:["Jan","Feb","Mar","Apr","May"],
+    datasets: [
+        {
+            label:"Income",
+            data: [2400,2800,3000,3400,3000],
+            borderColor: "#4CAF50",
+            backgroundColor: "#23232323",
+            tension: 0.5
+        },
+        {
+            label:"Expenses",
+            data: [1500,1800,2000,1600,1700],
+            borderColor: "#E53800",
+            backgroundColor: "#23232323",
+            tension: 0.5
+        }
+    ]
+}
+
+const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend : {
+            position: "top" as const,
+            labels: { color:"#f8f8f8"}
+        },
+        title: {
+            display : true,
+            text: "Income vs Expenses",
+            color: "#f8f8f8"
+        }
+    },
+    scales: {
+        x: {ticks: {color: "#aaa" , grid: {color:"#2323"}}},
+        y: {ticks: {color: "#aaa", grid: {color:"#2323"}}}
+    }
+}
+
 function IncomeExpensesChart(){
     return (
-        <>
-        Line Graph
-        </>
+        <div className="bg-[#1E1E1E]  p-6 rounded-2xl h-130 w-full mx-20" >
+            <Line data={chartData} options={chartOptions}/>
+        </div>
     )
 }
 
