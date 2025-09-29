eeb938e5753420ab737f4cf48da4867dfedb282b
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2025-09-29 03:02:33 -0400

    Add: { Adding budget page to App.tsx }

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import Login from "./pages/login";
 import Register from "./pages/register"
 import Dashboard from "./pages/dashboard";
 import Transactions from "./pages/transactions"
+import Budget from "./pages/budget"
+
+
 function app(){
 
     
@@ -14,6 +17,7 @@ function app(){
                     <Route path="/register" element={<Register />}></Route>
                     <Route path="/dashboard" element={<Dashboard />}></Route>
                     <Route path="/transactions" element={<Transactions />}></Route>
+                    <Route path="/budget" element={<Budget />}></Route>
                 </Routes>
             
         </Router>
