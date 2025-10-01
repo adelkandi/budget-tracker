bbafb624abda216aeb04d9a7710475fbf078810c
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2025-10-01 06:51:19 -0400

    Mod: {Redirect user when click on logo to /dashboard page}

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,13 +1,14 @@
 import Logo from "../../assets/BudgetTracker.svg"
+import {useNavigate} from "react-router-dom"
 function Navbar(){
-
+    const navigate = useNavigate()
  return (
     <nav className="navbar bg-zinc-900 text-[#F8F8FF]">
-        <div className="max-w-8xl mx-auto px-4 ms:px-6 lg:px-8">
+        <div className="max-w-8xl mx-auto px-4 ms:px-6 lg:px-8">  
             <div className="nav-container flex items-center justify-between h-20 relative">
                 {/* Logo */}
-                <div className="flex items-center gap-2">
-                    <img src={Logo} alt="Budget tracker Logo" className="h-16 w-auto cursor-pointer" onClick={() =>{console.log('clicked')}}/> {/* test */}
+                <div className="flex items-center gap-2"> 
+                    <img src={Logo} alt="Budget tracker Logo" className="h-16 w-auto cursor-pointer" onClick={() => navigate("/dashboard")}/> {/* test */}
                 </div>
 
                 {/* Links */}
