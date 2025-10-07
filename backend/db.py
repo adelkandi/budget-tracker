3eb28cdbe3aacf9aba2d49fa79106ab46aaf2cd8
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2025-10-07 00:27:51 -0400

    Add: {Added a fuction to create user in db.py}

--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,6 @@
 import os, mariadb
 from dotenv import load_dotenv
+from argon2 import PasswordHasher as ph
 
 load_dotenv()  
 
@@ -16,6 +17,14 @@ def get_conn():
     return mariadb.connect(**CFG)
 
 # Function to retreive username and password data to check for user: 
+
+password_hash = ph(
+    time_cost = 3,
+    memory_cost=65536,
+    parallelism=2,
+    hash_len=32
+)
+
 def check_user(username, password):
     conn = get_conn()
     cursor = conn.cursor()
@@ -24,3 +33,22 @@ def check_user(username, password):
     conn.close()
     return user is not None
 
+# Function to create users:
+
+def create_user(name,username,email,password):
+    conn = get_conn()
+    cursor = conn.cursor()
+    password_hashed = password_hash.hash(password)
+    try: 
+        cursor.execute("INSERT TO (name, username, email, password_hash) VALUES(%s,%s,%s,%s)",(name, username, email, password_hashed))
+        conn.commit()
+        return True 
+    except Exception as e:
+        print("Error craeting user: ", e)
+        conn.rollback()
+        return False
+    finally:
+        cursor.close()
+        conn.close()
+        
+    
