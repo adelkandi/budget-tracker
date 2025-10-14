2101cc17b65d5267da9bcb6d2f2058f6e1d42937
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2025-10-14 01:30:19 -0400

    fix: {Changed some variables names to match the data}

--- frontend/src/components/cards/RegisterCard.tsx
+++ frontend/src/components/cards/RegisterCard.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 
 
 function RegisterCard(){
-    const [fname, setFname] = useState("")
+    const [name, setname] = useState("")
     const [username, setUsername] = useState("")
     const [email, setEmail] = useState("")
     const [password,setPassword] = useState('')
@@ -14,7 +14,7 @@ function RegisterCard(){
         const response = await fetch("http://localhost:5000/register" ,{
             method:"POST",
             headers: {"Content-type": "application/json"},
-            body:JSON.stringify({fname,username,email,password})
+            body:JSON.stringify({name,username,email,password})
         })
         if (response.ok){
             navigate("/dashboard")
@@ -26,41 +26,47 @@ function RegisterCard(){
 
     // Register Card:
     return(
-        <div className="container">
-            <div className="cart-box">
-                
-                    {/* Logo */}
-                    <img className="logo" src={Logo} alt="Budget Tracker Logo" />
+        <form onSubmit={handleregister}>
+            <div className="container">
+                <div className="cart-box">
+                    
+                        {/* Logo */}
+                        <img className="logo" src={Logo} alt="Budget Tracker Logo" />
 
-                    {/* Title */}
-                    <h1 className="title">Register</h1>
+                        {/* Title */}
+                        <h1 className="title">Register</h1>
 
-                    {/* Full Name */}
-                    <label htmlFor="fname"></label>
-                    <input className="card-form" type="text" placeholder=" Full Name " name="fname" required/> 
+                        {/* Full Name */}
+                        <label htmlFor="fname"></label>
+                        <input className="card-form" type="text" placeholder=" Full Name " value={name} onChange={(e) => {setname(e.target.value)}} required/> 
 
-                    {/* UserName */}
-                    <label htmlFor="uname"></label>
-                    <input className="card-form" type="text" placeholder=" Username " name="uname" required/> 
+                        {/* UserName */}
+                        <label htmlFor="uname"></label>
+                        <input className="card-form" type="text" placeholder=" Username " value={username} onChange={(e) => {setUsername(e.target.value)}} required/>
 
-                    {/* Password */}
-                    <label htmlFor="psw"></label>
-                    <input className="card-form" type="password" placeholder=" Password " name="psw" required/> 
+                        {/* Email  */}
+                        <label htmlFor="email"></label>
+                        <input className="card-form" type="email" placeholder=" Email " value={email} onChange={(e) => {setEmail(e.target.value)}}/>
 
-                    {/* RePassword */}
-                    <label htmlFor="repsw"></label>
-                    <input className="card-form" type="password" placeholder=" Re-Password " name="repsw" required/> 
+                        {/* Password */}
+                        <label htmlFor="psw"></label>
+                        <input className="card-form" type="password" placeholder=" Password " value={password} onChange={(e) => {setPassword(e.target.value)}} required/> 
 
-                    {/* Submit Button */}
-                    <button type="submit" className="button inline-flex items-center justify-center px-6 py-2 rounded-lg
-                            bg-blue-700 text-white font-semibold
-                            hover:bg-blue-700 active:bg-blue-800
-                            focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2
-                            transition">Submit
-                    </button>
-                
+                        {/* RePassword */}
+                        <label htmlFor="repsw"></label>
+                        <input className="card-form" type="password" placeholder=" Re-Password " name="repsw" required/> 
+
+                        {/* Submit Button */}
+                        <button type="submit" className="button inline-flex items-center justify-center px-6 py-2 rounded-lg
+                                bg-blue-700 text-white font-semibold
+                                hover:bg-blue-700 active:bg-blue-800
+                                focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2
+                                transition">Submit
+                        </button>
+                    
+                </div>
             </div>
-        </div>
+        </form>
     )
 }
 export default RegisterCard
